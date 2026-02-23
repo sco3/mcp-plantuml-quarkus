c3b4dbe1a3563b38961e0d51eee6eff1c541506d
Dmitry Zakharov <zakharov@ibm.com>
2026-02-23 20:38:45 +0000

    upd

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 set -xueo pipefail
 
-jar=$(find $(dirname $(readlink -f $0)) -name math-tool-\*-runner.jar | grep -v /gen/ )
+jar=$(find $(dirname $(readlink -f $0))/build/ -name code-with-quarkus-\*-runner.jar | grep -v /gen/ )
 
 
 java -jar "$jar" --quiet 
